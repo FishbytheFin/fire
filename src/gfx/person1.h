d16598eeadc30e42e0750b4272e6414b8a14f542
BuildTools <unconfigured@null.spigotmc.org>
2022-05-08 07:23:00 -0700

    added person1 sprite

---
+++ src/gfx/person1.h
@@ -0,0 +1,18 @@
+#ifndef person1_include_file
+#define person1_include_file
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define person1_width 32
+#define person1_height 32
+#define person1_size 1026
+#define person1 ((gfx_sprite_t*)person1_data)
+extern unsigned char person1_data[1026];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
