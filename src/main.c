e391ae993a8407c6eb150a79aaf72f66d2c71999
BuildTools <unconfigured@null.spigotmc.org>
2022-05-07 21:59:05 -0700

    created 'drawScreen()' function

--- src/main.c
+++ src/main.c
@@ -5,29 +5,30 @@
 
 int main(void)
 {
-    /* Initialize graphics drawing */
+    /* Initialization */
     gfx_Begin();
-
-    /* Draw to buffer instead of directly to screen */
-    /* This is the same as gfx_SetDraw(gfx_buffer) */
     gfx_SetDrawBuffer();
-
     gfx_SetPalette(global_palette, sizeof_global_palette, 0);
-
     gfx_SetTransparentColor(0);
+
+    /*Drawing*/
+    drawScreen();
+
+    /* Waits for a key */
+    while (!os_GetCSC())
+        ;
+
+    gfx_End();
+
+    return 0;
+}
+
+void drawScreen()
+{
     gfx_FillScreen(1);
 
     gfx_TransparentSprite_NoClip(building, 0, 0);
     gfx_TransparentSprite_NoClip(floor, 72, 135);
 
     gfx_BlitBuffer();
-
-    /* Waits for a key */
-    while (!os_GetCSC())
-        ;
-
-    /* End graphics drawing */
-    gfx_End();
-
-    return 0;
 }
