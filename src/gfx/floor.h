ad846d35090ef9fccf44fb7b60230f7bdc7a3424
BuildTools <unconfigured@null.spigotmc.org>
2022-05-07 21:33:28 -0700

    added background sprites

---
+++ src/gfx/floor.h
@@ -0,0 +1,18 @@
+#ifndef floor_include_file
+#define floor_include_file
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define floor_width 248
+#define floor_height 105
+#define floor_size 26042
+#define floor ((gfx_sprite_t*)floor_data)
+extern unsigned char floor_data[26042];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
