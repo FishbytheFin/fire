0ca8b92b3066c504fd9f75b1c3ed533b195b0504
BuildTools <unconfigured@null.spigotmc.org>
2022-05-09 22:43:32 -0700

    fixed sprite rendering issues

--- src/main.c
+++ src/main.c
@@ -5,7 +5,6 @@
 #include "gfx/gfx.h"
 
 int score;
-int gameVersion;
 int playerPos = 0;
 int people[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
@@ -76,18 +75,18 @@ int drawScreen()
     gfx_FillScreen(1);
 
     gfx_TransparentSprite_NoClip(building, 0, 0);
-    gfx_TransparentSprite_NoClip(floor1, 96, 211);
-    gfx_TransparentSprite_NoClip(floor2, 260, 171);
+    gfx_TransparentSprite_NoClip(floor1, 76, 211);
+    gfx_TransparentSprite_NoClip(floor2, 257, 142);
 
     gfx_TransparentSprite_NoClip(person1, 20, 38);
-    gfx_TransparentSprite_NoClip(person2, 50, 38);
-    gfx_TransparentSprite_NoClip(person3, 80, 38);
-    gfx_TransparentSprite_NoClip(person4, 110, 38);
-    gfx_TransparentSprite_NoClip(person5, 140, 38);
+    gfx_TransparentSprite_NoClip(person2, 37, 75);
+    gfx_TransparentSprite_NoClip(person3, 42, 112);
+    gfx_TransparentSprite_NoClip(person4, 45, 147);
+    gfx_TransparentSprite_NoClip(person5, 54, 174);
 
-    gfx_TransparentSprite_NoClip(crash, 28, 200);
+    gfx_TransparentSprite_NoClip(crash, 36, 208);
 
-    gfx_TransparentSprite_NoClip(player, 28 + (playerPos * 79), 177);
+    gfx_TransparentSprite_NoClip(player, 28 + (playerPos * 75), 177);
 
     gfx_BlitBuffer();
     return 0;
